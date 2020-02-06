cfd3298713304a3447763538d721a69af0036e31
Candace <2100978@talnet.nl>
2020-02-06 12:54:03 +0100

    Uitwerking van de exercise

--- meer-topo.php
+++ meer-topo.php
@@ -1,44 +1,7 @@
 
 <?php
 
-$quiz = array(
-    "Japan" => "Tokyo",
-    "Mexico" => "Mexico City",
-    "USA" => "Washington D.C",
-    "India" => "New Delhi",
-    "Zuid-Korea" => "Seoul",
-    "China" => "Peking",
-    "Nigeria" => "Abuja",
-    "Argentina" => "Buenos Aires",
-    "UK" => "London",
-    "Egypt" => "Cairo",
 
-);
-
-
-// $g = 0;
-// foreach ($quiz as $land => $hoofdstad) {
-//     if ($c === true) {
-//         $g++;
-//     }
-
-
-//     $c = false;
-//     $country = readline("welke hoofdstad heeft $land?");
-
-//     if ($country == $hoofdstad) {
-
-//         echo "Correct!" . PHP_EOL;
-//         $c = true;
-//     } else {
-
-//         echo "Helaas $country is niet de hoofdstad van $land" . PHP_EOL;
-//         echo "Het Correcte antwoord is: $hoofdstad" . PHP_EOL;
-//     }
-// }
-
-// echo "Je hebt " . $g . " van de  " . count($quiz) . "  goed geraden" . PHP_EOL;
-// exit;
 $database = [];
 
 echo ("Hoeveel landen wil je toevoegen?" . PHP_EOL);
@@ -61,11 +24,3 @@ foreach ($database as $kseya => $valuea) {
 
     echo $kseya . "," . $valuea . PHP_EOL;
 }
-
-// foreach ($database as $nname => $valss) {
-
-
-//     echo $nname . "," . $valss . PHP_EOL;
-// }
-
-print_r($database);
